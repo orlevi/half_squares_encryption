c5c97c1a3916071720af38e538bcc56577e743dd
Or Levi <orlevi1234@gmail.com>
2017-11-15 09:02:23 +0200

    remove non vectoric images code

--- half_squares_encryption.py
+++ half_squares_encryption.py
@@ -4,33 +4,27 @@ import random
 class half_squares:
     def __init__(self):
         self.pixel_size = 1 #mm
-        self.triangle_0 = [(0,0),(1,1),(1,0)]
-        self.triangle_1 = [(0,0),(1,1),(0,1)]
-        self.triangle_2 = [(0,1),(1,0),(0,0)]
-        self.triangle_3 = [(0,1),(1,0),(1,1)]
+        self.triangle_0 = [(0,0),(1,1),(1,0)] #upper right corner
+        self.triangle_1 = [(0,0),(1,1),(0,1)] #lower left corner
+        self.triangle_2 = [(0,1),(1,0),(0,0)] #upper left corner
+        self.triangle_3 = [(0,1),(1,0),(1,1)] #lower right corner
         self.triangles = [self.triangle_0, self.triangle_1, self.triangle_2, self.triangle_3]
 
-        self.image_0 = Image.open(r"half_square_0_i.png")
-        self.image_1 = Image.open(r"half_square_1_i.png")
-        self.image_2 = Image.open(r"half_square_2_i.png")
-        self.image_3 = Image.open(r"half_square_3_i.png")
         self.input_image = Image.open(r"name7.png")
         self.input_image_conv = self.input_image.convert("1")
         self.input_image_data =  list(self.input_image_conv.getdata())
         self.row_num , self.column_num = self.input_image.size
         self.rand_pattern = [[True for i in range(self.column_num)] for j in range(self.row_num)]
         self.comp_pattern = [[True for i in range(self.column_num)] for j in range(self.row_num)]
-        self.rand_pattern_image = Image.new("1",(self.row_num * 5, self.column_num * 5))
-        self.comp_pattern_image = Image.new("1",(self.row_num * 5, self.column_num * 5))
-        self.comp_flipped_image = Image.new("1",(self.row_num * 5, self.column_num * 5))
 
         self.vectoric_rand = open(r"vectoric_rand.svg",'w')
         self.vectoric_comp = open(r"vectoric_comp.svg",'w')
         self.vectoric_comp_flipped = open(r"vectoric_comp_flipped.svg",'w')
-        header = '<svg \n\twidth="{}mm"\n\theight="{}mm"\n\tviewBox="0 0 {} {}"\n\tversion="1.1" >\n'.format(self.row_num * self.pixel_size,
-                                                                                                           self.column_num * self.pixel_size,
-                                                                                                           self.row_num * self.pixel_size,
-                                                                                                           self.column_num * self.pixel_size)
+        header = '<svg \n\twidth="{}mm"\n\theight="{}mm"\n\tviewBox="0 0 {} {}"\n\tversion="1.1" >\n'.format(
+            self.row_num * self.pixel_size,
+            self.column_num * self.pixel_size,
+            self.row_num * self.pixel_size,
+            self.column_num * self.pixel_size)
         self.vectoric_rand.write(header)
         self.vectoric_comp.write(header)
         self.vectoric_comp_flipped.write(header)
@@ -71,39 +65,27 @@ class half_squares:
         for row in range(self.row_num):
             for col in range(self.column_num):
                 if self.rand_pattern[row][col] == 0:
-                    self.rand_pattern_image.paste(self.image_0, (row * 5, col * 5))
                     self.vectoric_rand.write(self.get_svg_triangle(0,row,col))
                 elif self.rand_pattern[row][col] == 1:
-                    self.rand_pattern_image.paste(self.image_1, (row * 5, col * 5))
                     self.vectoric_rand.write(self.get_svg_triangle(1, row, col))
                 elif self.rand_pattern[row][col] == 2:
-                    self.rand_pattern_image.paste(self.image_2, (row * 5, col * 5))
                     self.vectoric_rand.write(self.get_svg_triangle(2, row, col))
                 elif self.rand_pattern[row][col] == 3:
-                    self.rand_pattern_image.paste(self.image_3, (row * 5, col * 5))
                     self.vectoric_rand.write(self.get_svg_triangle(3, row, col))
 
                 if self.comp_pattern[row][col] == 0:
-                    self.comp_pattern_image.paste(self.image_0, (row * 5, col * 5))
                     self.vectoric_comp.write(self.get_svg_triangle(0, row, col))
                     self.vectoric_comp_flipped.write(self.get_svg_triangle(0, row, col, 1))
                 elif self.comp_pattern[row][col] == 1:
-                    self.comp_pattern_image.paste(self.image_1, (row * 5, col * 5))
                     self.vectoric_comp.write(self.get_svg_triangle(1, row, col))
                     self.vectoric_comp_flipped.write(self.get_svg_triangle(1, row, col, 1))
                 elif self.comp_pattern[row][col] == 2:
-                    self.comp_pattern_image.paste(self.image_2, (row * 5, col * 5))
                     self.vectoric_comp.write(self.get_svg_triangle(2, row, col))
                     self.vectoric_comp_flipped.write(self.get_svg_triangle(2, row, col, 1))
                 elif self.comp_pattern[row][col] == 3:
-                    self.comp_pattern_image.paste(self.image_3, (row * 5, col * 5))
                     self.vectoric_comp.write(self.get_svg_triangle(3, row, col))
                     self.vectoric_comp_flipped.write(self.get_svg_triangle(3, row, col, 1))
-        self.comp_flipped_image = self.comp_pattern_image.transpose(Image.FLIP_LEFT_RIGHT)
 
-        self.comp_pattern_image.save(r"C:\Users\Or Levi\PycharmProjects\half_squares_encryption\comp.png")
-        self.comp_flipped_image.save(r"C:\Users\Or Levi\PycharmProjects\half_squares_encryption\comp_flipped.png")
-        self.rand_pattern_image.save(r"C:\Users\Or Levi\PycharmProjects\half_squares_encryption\rand.png")
         self.vectoric_rand.write("</svg>")
         self.vectoric_comp.write("</svg>")
         self.vectoric_comp_flipped.write("</svg>")
